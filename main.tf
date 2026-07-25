476cd876ff6823c003398fffd45e179b50b3cebb
Ma-test25 <mahindha2192@gmail.com>
2026-07-26 00:09:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB"
+resource "snowflake_database" "demo_data" {
+  name    = "DEMO_DATA"
   comment = "Database for Snowflake Terraform demo"
 }
